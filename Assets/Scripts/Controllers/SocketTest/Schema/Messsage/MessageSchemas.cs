ada3755cfa1865e3db76dc49728f977ad8112c7e
lehoangvuvt <hoangvule100@gmail.com>
2022-05-05 15:41:32 +0700

    fix

--- Assets/Scripts/Controllers/SocketTest/Schema/Messsage/MessageSchemas.cs
+++ Assets/Scripts/Controllers/SocketTest/Schema/Messsage/MessageSchemas.cs
@@ -5,17 +5,5 @@ using Colyseus.Schema;
 
 public class MessageSchemas
 {
-    public void getMessageSchemaByType(string messageType)
-    {
-        var messageSchema = new Schema();
-        switch(messageType)
-        {
-            case MESSAGE_TYPE.ROLL_RESULT:
-                messageSchema = new RollResultMessage();
-                break;
-            default:
-                break;
-        }
-        return messageSchema;
-    }
+ 
 }

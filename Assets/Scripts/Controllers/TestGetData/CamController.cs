ba8c8b7b669436cbbc5288da94ebee7e39f92d77
Vũ Lê <lehoangvu@bacoor.co>
2022-07-16 13:27:06 +0700

    fix formation

--- Assets/Scripts/Controllers/TestGetData/CamController.cs
+++ Assets/Scripts/Controllers/TestGetData/CamController.cs
@@ -5,15 +5,27 @@ public class CamController : MonoBehaviour
     public float speed = 3.5f;
     private float X;
     private float Y;
+    [SerializeField]
+    private GameObject board;
 
     void Update()
     {
         if (Input.GetMouseButton(0))
         {
-            transform.Rotate(new Vector3(Input.GetAxis("Mouse Y") * speed, -Input.GetAxis("Mouse X") * speed, 0));
-            X = transform.rotation.eulerAngles.x;
-            Y = transform.rotation.eulerAngles.y;
-            transform.rotation = Quaternion.Euler(0, Y, 0);
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.transform.name == gameObject.name)
+                {
+                    board.transform.Rotate(new Vector3(Input.GetAxis("Mouse Y") * speed, -Input.GetAxis("Mouse X") * speed, 0));
+                    X = board.transform.rotation.eulerAngles.x;
+                    Y = board.transform.rotation.eulerAngles.y;
+                    board.transform.rotation = Quaternion.Euler(0, Y, 0);
+                }
+            }
+
         }
     }
 }
